a5678ce99bf28539a5c65f6b1bc61c12de2b5226
vladmiass <vladmiass01012001@gmail.com>
2023-08-31 22:42:36 +0500

    home work, ДЗ4, доработал круговой

--- Seminar4/task24.py
+++ Seminar4/task24.py
@@ -3,8 +3,11 @@ arr = list()
 for i in range(n):
     a =int(input('Введите количество ягод на кусте: '))
     arr.append(a)
+arr.append(arr[0])
+arr.append(arr[1])
 arr_count = list()
 for i in range(1, len(arr) - 1):
        arr_count.append(arr[i - 1] + arr[i] + arr[i + 1 ])
        print(i, arr_count)
 print(max(arr_count))
+print(arr)

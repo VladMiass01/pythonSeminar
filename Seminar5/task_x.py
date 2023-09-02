b5ef7aadb520f2da104057db3c55a68c0041962c
vladmiass <vladmiass01012001@gmail.com>
2023-09-03 00:19:42 +0500

    home work, ДЗ5, добавил холодильник

---
+++ Seminar5/task_x.py
@@ -0,0 +1,27 @@
+#code = {1:'222anton456',
+#    2:'a1n1t1o1n1',
+#    3:'0000a0000n00t00000o000000n',
+#    4:'gylfole',
+#    5:'richard',
+#    6:'ant0n'}
+#1 2 3
+code = {1:'osfjwoiergwoignaewpjofwoeijfnwfonewfoignewtowenffnoeiwowjfninoiwfen',
+    2:'anton',
+    3:'aoooooooooontooooo',
+    4:'elelelelelelelelelel',
+    5:'ntoneeee',
+    6:'tonee',
+    7:'253235235a5323352n25235352t253523523235oo235523523523n',
+    8:'antoooooooooooooooooooooooooooooooooooooooooooooooooooon',
+    9:'unton'}
+#1 2 7 8
+virus = 'anton'
+for k in range(1, len(code) + 1):
+    s = code[k]
+    res = ''
+    for m in virus:
+        if m in s:
+            res = res + m
+            s = s[s.find(m):]
+    if res == virus:
+        print(k)

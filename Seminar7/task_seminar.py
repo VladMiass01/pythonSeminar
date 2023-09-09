69c8f046d046b9c513bac8a25324f5e5f21ec944
vladmiass <vladmiass01012001@gmail.com>
2023-09-09 10:23:42 +0500

    подправил сумму квадратов

--- Seminar7/task_seminar.py
+++ Seminar7/task_seminar.py
@@ -60,9 +60,10 @@
 
 sum1 = 0
 sum2 = 0
-temp = lambda x: x * x * (x % 9 == 0)
+# temp = lambda x: x * x * (x % 9 == 0)
 for i in range(10, 100):
-    sum1 = sum1 + temp(i)
+    # sum1 = sum1 + temp(i)
+    sum1 = sum1 + (lambda x: x * x * (x % 9 == 0)) (i)
     sum2 = sum2 + i * i * (i % 9 == 0)
 
 print(sum1, "  ", sum2)
